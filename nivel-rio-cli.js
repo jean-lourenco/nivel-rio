133cd790260a37dda1094d738503a5a9e9c1dccd
Jean Lourenço <jeancarlos.bnu@gmail.com>
2017-08-08 20:03:09 -0300

    Added 'recent measurements' feature

--- nivel-rio-cli.js
+++ nivel-rio-cli.js
@@ -6,19 +6,58 @@ const nivelRioLib = require('./nivel-rio-lib');
 const chalk = require('chalk');
 
 const cli = meow(`
-    Usage
+    Utilização
         $ nivel-rio    
-`);
 
-nivelRioLib
-    .getAllRiverLevelInfo()
-    .then((x) => console.log(`
-        ${x[0].date.format("YYYY-MM-DD HH:mm")} - ${x[0].level} meters (${getLevelDifference(x[3].level, x[0].level)})
-    `));
+    Opções
+        --recente, -r    lista as 10 medições mais recentes por hora
 
+    Exemplos
+        $ nivel-rio
+
+            2017-08-08 18:00 - 1.29 metros (-0.08)
+
+        $ nivel-rio -r
+
+            2017-08-08 19:00 - 1.07 metros (-0.22)
+            2017-08-08 18:00 - 1.29 metros (-0.08)
+            ...
+            2017-08-08 11:00 - 0.43 metros (-0.06)
+            2017-08-08 10:00 - 0.49 metros (-0.11)
+
+`, {
+    alias: {
+        r: 'recente'
+    }
+});
+
+const flags = cli.flags;
+
+if (flags['recente']) {
+    nivelRioLib
+        .getAllRiverLevelInfo()
+        .then((x) => showMeasurementPerHour(x, 10));    
+} else {
+    nivelRioLib
+        .getAllRiverLevelInfo()
+        .then((x) => showMeasurementPerHour(x, 1));
+}
 
 function getLevelDifference(originalLevel, actualLevel) {
     var difference = (actualLevel - originalLevel).toFixed(2);
 
     return difference > 0 ? chalk.red(`+${difference}`) : chalk.green(difference);
+}
+
+function showMeasurementPerHour(measurements, quantity) {
+    let riverIndex = 0;
+    console.log('');
+
+    while (0 < quantity--) {
+        console.log(`${measurements[riverIndex].date.format("YYYY-MM-DD HH:mm")} - ${measurements[riverIndex].level} metros (${getLevelDifference(measurements[riverIndex + 4].level, measurements[riverIndex].level)})`);
+
+        riverIndex += 4;
+    }
+
+    console.log('');
 }
